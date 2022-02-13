3cd47c5932a77be0c4f54f660b739f39035f5776
USCgil0127 <0420990@gmail.com>
2022-02-13 16:18:28 +0900

    검색 폼 & 검색 결과 구현 완료

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -9,15 +9,37 @@ class App extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log("TODO: handleSubmit", this.state.searchKeyword);
+    console.log(
+      "TODO: handleSubmit 제출시 검색어 : ",
+      this.state.searchKeyword
+    );
+    // this.setState({ searchKeyword: "" });
+
+    this.handleReset();
   }
 
   handleReset() {
-    // TODO
+    this.setState(
+      () => {
+        return { searchKeyword: "" };
+      },
+      () => {
+        console.log(
+          "setState()가 완료 되는 시점에 돌아오는 콜백 함수 값 : ",
+          this.state.searchKeyword
+        );
+      }
+    );
   }
 
   handleChangeInput(event) {
     const searchKeyword = event.target.value;
+
+    // 검색어가 삭제됐다고 판단되면, 리셋버튼과 똑같은 효과를 냄
+    if (searchKeyword.length <= 0) {
+      return this.handleReset();
+    }
+
     this.setState({ searchKeyword });
   }
 
@@ -30,6 +52,7 @@ class App extends React.Component {
         <div className="container">
           <form
             onSubmit={(event) => this.handleSubmit(event)}
+            onReset={(event) => this.handleReset()}
             // TODO
           >
             <input
